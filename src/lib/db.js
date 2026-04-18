6cd9f68f96979fd26f0b9d13c508e90b5847bbeb
vmguptamc25-lang <vmgupta_mc25@mc.vjti.ac.in>
2026-04-18 17:02:25 +0530

    Throw error if MONGO_URL is not set
    Added error handling for missing MONGO_URL environment variable.

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,15 +1,20 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGO_URL; // Local MongoDB URI
+const uri = process.env.MONGO_URL;
 const options = {};
 
 let client;
 let clientPromise;
 
+if (!uri) {
+  throw new Error("Please add MONGO_URL to environment variables");
+}
+
 if (!global._mongoClientPromise) {
   client = new MongoClient(uri, options);
-  global._mongoClientPromise = client.connect();
+  global._mongoClientPromise = client.connect(); // still OK (lazy promise)
 }
 
 clientPromise = global._mongoClientPromise;
+
 export default clientPromise;
